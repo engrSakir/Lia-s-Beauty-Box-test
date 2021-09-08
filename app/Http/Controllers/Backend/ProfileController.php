8cdb40c65dcd6f0897ce094a8ce45ae27bf80f7f
moumitazaman <moumitasub@gmail.com>
2021-09-08 19:28:02 +0600

    Profile Update

---
+++ app/Http/Controllers/Backend/ProfileController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+use Illuminate\Support\Str;
+
+class ProfileController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $id=auth()->user()->id;
+        $user= User::where('id',$id)->first();
+
+        return view('backend.profile.index',['user' => $user]);
+
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $id=auth()->user()->id;
+        
+        $update = User::find($id);
+
+        if ($request->file('image')) {
+            $imagePath = $request->file('image');
+            $imageName = $imagePath->getClientOriginalName();
+            $path = $request->file('image')->storeAs('uploads', $imageName, 'public');
+            $request->image->move(public_path('/uploads'), $imageName);
+            $update->img_name =$imageName;
+    
+    
+          }
+    
+          $update->name = $request->input('name');
+          $update->email = $request->input('email');
+          $update->phone = $request->input('phone');
+          $update->save();
+         // $request->session()->flash('success', 'Successfully Updated!');
+                return redirect(route('profile'));
+    
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+       
+
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
