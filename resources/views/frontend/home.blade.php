bd9e06553bed2c328a2ca41efbff315500542b9b
moumitazaman <moumitasub@gmail.com>
2021-09-15 16:52:45 +0600

    Client added to frontend

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -2901,103 +2901,15 @@
             <!-- IMAGE CAROUSEL START -->
             <div class="section-content">
                 <div class="owl-carousel client-logo-carousel">
-
-                    <!-- COLUMNS 1 -->
+                    @foreach($clients as $client)
                     <div class="item">
                         <div class="ow-client-logo">
                             <div class="client-logo wt-img-effect on-color">
-                                <a href="javascript:;"><img src="images/client-logo/logo1.png" alt=""></a>
+                                <a href="javascript:;"><img src="{{ asset($client->logo ?? 'uploads/images/no_image.png') }}" alt=""></a>
                             </div>
                         </div>
                     </div>
-                    <!-- COLUMNS 2 -->
-                    <div class="item">
-                        <div class="ow-client-logo">
-                            <div class="client-logo wt-img-effect on-color">
-                                <a href="javascript:;"><img src="images/client-logo/logo2.png" alt=""></a>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- COLUMNS 3 -->
-                    <div class="item">
-                        <div class="ow-client-logo">
-                            <div class="client-logo wt-img-effect on-color">
-                                <a href="javascript:;"><img src="images/client-logo/logo3.png" alt=""></a>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- COLUMNS 4 -->
-                    <div class="item">
-                        <div class="ow-client-logo">
-                            <div class="client-logo wt-img-effect on-color">
-                                <a href="javascript:;"><img src="images/client-logo/logo4.png" alt=""></a>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- COLUMNS 5 -->
-                    <div class="item">
-                        <div class="ow-client-logo">
-                            <div class="client-logo wt-img-effect on-color">
-                                <a href="javascript:;"><img src="images/client-logo/logo5.png" alt=""></a>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- COLUMNS 6 -->
-                    <div class="item">
-                        <div class="ow-client-logo">
-                            <div class="client-logo wt-img-effect on-color">
-                                <a href="javascript:;"><img src="images/client-logo/logo6.png" alt=""></a>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- COLUMNS 7 -->
-                    <div class="item">
-                        <div class="ow-client-logo">
-                            <div class="client-logo wt-img-effect on-color">
-                                <a href="javascript:;"><img src="images/client-logo/logo7.png" alt=""></a>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- COLUMNS 8 -->
-                    <div class="item">
-                        <div class="ow-client-logo">
-                            <div class="client-logo wt-img-effect on-color">
-                                <a href="javascript:;"><img src="images/client-logo/logo8.png" alt=""></a>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- COLUMNS 9 -->
-                    <div class="item">
-                        <div class="ow-client-logo">
-                            <div class="client-logo wt-img-effect on-color">
-                                <a href="javascript:;"><img src="images/client-logo/logo9.png" alt=""></a>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- COLUMNS 10 -->
-                    <div class="item">
-                        <div class="ow-client-logo">
-                            <div class="client-logo wt-img-effect on-color">
-                                <a href="javascript:;"><img src="images/client-logo/logo10.png" alt=""></a>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- COLUMNS 11 -->
-                    <div class="item">
-                        <div class="ow-client-logo">
-                            <div class="client-logo wt-img-effect on-color">
-                                <a href="javascript:;"><img src="images/client-logo/logo11.png" alt=""></a>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- COLUMNS 12 -->
-                    <div class="item">
-                        <div class="ow-client-logo">
-                            <div class="client-logo wt-img-effect on-color">
-                                <a href="javascript:;"><img src="images/client-logo/logo12.png" alt=""></a>
-                            </div>
-                        </div>
-                    </div>
+                    @endforeach
 
                 </div>
             </div>
