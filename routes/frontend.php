908ab78968019f9aff3095fe18dfdf7c67b7a849
moumitazaman <moumitasub@gmail.com>
2021-09-24 22:12:28 +0600

    Salary update

--- routes/frontend.php
+++ routes/frontend.php
@@ -14,4 +14,6 @@ Route::get('/booking', [FrontEndController::class, 'booking'])->name('booking');
 Route::post('/booking', [FrontEndController::class, 'bookingStore']);
 Route::get('/service', [FrontEndController::class, 'service'])->name('service');
 Route::get('/service/{slug}', [FrontEndController::class, 'serviceDetails'])->name('serviceDetails');
+Route::post('/contact-us', [FrontEndController::class, 'handleContactForm']);
+
 
