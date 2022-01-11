46f813a32975b768507d173ecd6279ef17816815
Md. Sakir Ahmed <52326976+mdsakirahmed@users.noreply.github.com>
2022-01-11 11:57:29 +0600

    get appointment use livewire

--- app/Http/Livewire/Widgets/Appointment.php
+++ app/Http/Livewire/Widgets/Appointment.php
@@ -6,8 +6,28 @@ use Livewire\Component;
 
 class Appointment extends Component
 {
+    public $date, $schedules;
+
     public function render()
     {
+        if($this->date){
+            // $this->schedules = 'Yes';
+            $weekMap = [
+                0 => 'sunday',
+                1 => 'monday',
+                2 => 'tuesday',
+                3 => 'wednesday',
+                4 => 'thursday',
+                5 => 'friday',
+                6 => 'saturday',
+            ];
+            $day_name = $weekMap[date('w', strtotime($this->date))];
+            $this->schedules = [
+                'day_name' => $day_name,
+                'date' => $this->date,
+                'schedules' => \App\Models\Schedule::where('schedule_day', $day_name)->get()
+            ];
+        }
         return view('livewire.widgets.appointment');
     }
 }
