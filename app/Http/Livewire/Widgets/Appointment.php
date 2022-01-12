1a4b2d02872225cea9603d77468f56d00de1bb0a
Nahim Udoy <Nahimbinrezaudoy@gmail.com>
2022-01-12 17:04:40 +0600

    Work On Appointment

--- app/Http/Livewire/Widgets/Appointment.php
+++ app/Http/Livewire/Widgets/Appointment.php
@@ -6,11 +6,15 @@ use App\Models\Schedule;
 use App\Models\Service;
 use App\Models\ServiceCategory;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
+use Illuminate\Support\Str;
+use PDF;
+use Illuminate\Support\Facades\Mail;
 
 class Appointment extends Component
 {
-    public $date, $schedules, $selected_schedule, $services, $service_categories, $service_category, $searched_key_in_busket, $staffs;
+    public $date, $schedules, $selected_schedule, $services, $service_categories, $service_category, $searched_key_in_busket, $staffs, $admin_mode;
     public $basket = array();
 
     public function mount()
@@ -50,7 +54,143 @@ class Appointment extends Component
 
     public function store()
     {
-        dd($this->basket);
+        $this->validate([
+            'date'  => 'required|string',
+            'selected_schedule' => 'required|exists:schedules,id',
+        ]);
+        if (!$this->admin_mode) {
+            //Common Validation check for all user
+            $this->validate([
+                'message'           => 'nullable|string',
+                'transaction_id'     => 'required|string',
+                'advance_amount'   => 'required|numeric|min:' . get_static_option('advance_amount'),
+            ]);
+            if (!auth()->check()) {
+                //Validation check for guest user | Create user first
+                $this->validate(
+                    [
+                        'name'      => 'required|string',
+                        'email'     => 'nullable|email', //Not Unique because Email is Nullable
+                        'phone'     => 'required|unique:users,phone',
+                        'address'     => 'nullable|string',
+                        'message'   => 'nullable|string',
+                    ],
+                    [
+                        'phone.unique' => 'Already you have an account. Please login before order or use another phone.',
+                    ]
+                );
+                $password = Str::random(8);
+                $user = new User();
+                $user->name         = $this->name;
+                $user->email        = $this->email;
+                $user->phone        = $this->phone;
+                $user->address        = $this->address;
+                $user->password     = bcrypt($password);
+                $user->save();
+                $user->assignRole('Customer');
+            } else {
+                $user = Auth::user();
+            }
+        } else {
+            // Validattion For admin
+            $this->validate(
+                [
+                    'name'      => 'required|string',
+                    'email'     => 'nullable|email',
+                    'phone'     => 'required|string',
+                    'address'     => 'nullable|string',
+                ]
+            );
+            if ($this->email) {
+                $user = User::where('email', $this->email)->first();
+            } else if ($this->phone) {
+                $user = User::where('phone', $this->phone)->first();
+            } else {
+                $user = User::where('name', $this->name)->first();
+            }
+
+            $password = null;
+
+            if (!$user) {
+                $this->validate(
+                    [
+                        'name'      => 'required|string',
+                        'email'     => 'nullable|email', //Not Unique because Email is Nullable
+                        'phone'     => 'required|unique:users,phone',
+                        'address'     => 'nullable|string',
+                        'message'   => 'nullable|string',
+                    ],
+                    [
+                        'phone.unique' => 'This phone number already used. Please use another phone number.',
+                    ]
+                );
+                $password = Str::random(8);
+                $user = new User();
+                $user->password     = bcrypt($password);
+            }
+            //New save or auto update
+            $user->name         = $this->name;
+            $user->email        = $this->email;
+            $user->phone        = $this->phone;
+            $user->address      = $this->address;
+            $user->save();
+            if ($password) {
+                $user->assignRole('Customer');
+            }
+        }
+        //Create Schedule
+        $max_participent_in_this_day = Appointment::where('appointment_data', date('Y-m-d', strtotime($this->date)))
+            ->where('schedule_id',  $this->selected_schedule->id)
+            ->where('status', '!=', 'Reject')->count();
+        if ($max_participent_in_this_day > $this->selected_schedule->maximum_participant) {
+            //Not Available Caz Max Participent.
+            // return [
+            //     'type' => 'error',
+            //     'message' => 'Housefull',
+            // ];
+        } else {
+            // Available For New Appointment.
+            $appointment = new Appointment();
+            $appointment->customer_id       = $user->id;
+            $appointment->appointment_data  = date('Y-m-d', strtotime($this->date));
+            $appointment->schedule_id       = $this->selected_schedule;
+            $appointment->transaction_id    = $this->transaction_id;
+            $appointment->advance_amount    = $this->advance_amount ?? 0;
+            if ($this->admin_mode) {
+                $appointment->status            = 'Approved'; //Administritive auto approve 
+            }
+            $appointment->save();
+
+            //Successfully Save Appointment And Send Notification Alert.
+            if ($appointment->customer->email) {
+                try {
+                    $data = [
+                        'from'    => 'from@email.com',
+                        'to'      => $appointment->customer->email,
+                        'subject' => 'Booking Approved',
+                        'pdf'     => PDF::loadView('backend.invoice.advance-inv-pdf', compact('appointment')),
+                    ];
+                    Mail::send(
+                        'emails.adance_payment',
+                        ['data' => $data],
+                        function ($message) use ($data) {
+                            $message
+                                ->from($data['from'])
+                                ->to($data['to'])
+                                ->subject($data['subject'])
+                                ->attachData($data['pdf']->output(), 'invoice.pdf');
+                        }
+                    );
+                    // Success Alert of Email Sending
+                } catch (\Exception $exception) {
+                    // Error Alert of Email Not Sending
+                    // return response()->json([
+                    //     'type' => 'warning',
+                    //     'message' => 'Email problem ' . $exception->getMessage()
+                    // ]);
+                }
+            }
+        }
     }
 
     public function addToCard($id)
--- resources/views/backend/appointment/create.blade.php
+++ resources/views/backend/appointment/create.blade.php
@@ -3,185 +3,184 @@
 @section('title') Appointment Create @endsection
 
 @section('bread-crumb')
-    <div class="row page-titles">
-        <div class="col-md-5 align-self-center">
-            <h4 class="text-themecolor">Appointment Create Page</h4>
-        </div>
-        <div class="col-md-7 align-self-center text-end">
-            <div class="d-flex justify-content-end align-items-center">
-                <ol class="breadcrumb justify-content-end">
-                    <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
-                    <li class="breadcrumb-item active">Appointment Create Page</li>
-                </ol>
-                <a href="{{ route('backend.invoice.create') }}" class="btn btn-info d-none d-lg-block m-l-15"><i
-                        class="fa fa-plus-circle"></i>Direct Invoice</a>
-            </div>
+<div class="row page-titles">
+    <div class="col-md-5 align-self-center">
+        <h4 class="text-themecolor">Appointment Create Page</h4>
+    </div>
+    <div class="col-md-7 align-self-center text-end">
+        <div class="d-flex justify-content-end align-items-center">
+            <ol class="breadcrumb justify-content-end">
+                <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
+                <li class="breadcrumb-item active">Appointment Create Page</li>
+            </ol>
+            <a href="{{ route('backend.invoice.create') }}" class="btn btn-info d-none d-lg-block m-l-15"><i
+                    class="fa fa-plus-circle"></i>Direct Invoice</a>
         </div>
     </div>
+</div>
 @endsection
 
 @section('content')
-    @livewire('widgets.appointment')
-    <hr>
-    <div class="row">
-        <div class="col-lg-6">
-            <div class="card">
-                <div class="card-header bg-info">
-                    <h4 class="mb-0 text-white">Calendar</h4>
-                </div>
-                <div class="calendar-wrapper">{{-- Calender show using jQuery --}}</div>
+@livewire('widgets.appointment' , ['admin_mode' => true])
+<hr>
+<div class="row">
+    <div class="col-lg-6">
+        <div class="card">
+            <div class="card-header bg-info">
+                <h4 class="mb-0 text-white">Calendar</h4>
             </div>
+            <div class="calendar-wrapper">{{-- Calender show using jQuery --}}</div>
         </div>
-        <div class="col-lg-6">
-            <div class="card">
-                <div class="card-header bg-info">
-                    <h4 class="mb-0 text-white">Schedules</h4>
-                </div>
-                <div class="card-body">
-                    <div class="message-box">
-                        <div class="message-widget message-scroll" id="schedule">
-                            {{-- Schedules show using jQuery --}}
-                            <div class="alert alert-success text-center m-3"
-                                style="border: 2px solid red; padding: 10px; border-radius: 50px 20px;" role="alert">
-                                <h3>Please pick a date from calendar</h3>
-                            </div>
+    </div>
+    <div class="col-lg-6">
+        <div class="card">
+            <div class="card-header bg-info">
+                <h4 class="mb-0 text-white">Schedules</h4>
+            </div>
+            <div class="card-body">
+                <div class="message-box">
+                    <div class="message-widget message-scroll" id="schedule">
+                        {{-- Schedules show using jQuery --}}
+                        <div class="alert alert-success text-center m-3"
+                            style="border: 2px solid red; padding: 10px; border-radius: 50px 20px;" role="alert">
+                            <h3>Please pick a date from calendar</h3>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 
-    <div class="modal fade" id="booking_modal" aria-hidden="true" aria-labelledby="booking_modal" tabindex="-1">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header  bg-success text-white">
-                    <h5 class="modal-title" id="schedule_title">Title</h5>
-                    <hr>
-                    <b class="modal-title" id="schedule_date">Date</b> &nbsp; <b class="modal-title"
-                        id="schedule_time">Sub Title</b>
-                </div>
-                <div class="modal-body">
-                    <div class="m-a30 wt-box border-2">
-                        {{-- date value set by jQuery --}}
-                        <input type="hidden" id="appointment_data" name="appointment_data">
-                        <input type="hidden" id="schedule_id" name="schedule">
-                        <form class="cons-contact-form" id="appointment_form">
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <div class="form-group">
-                                        <div class="input-group">
-                                            <input name="name" type="text" required=""
-                                                class="form-control customer_information" placeholder="Name">
-                                        </div>
+<div class="modal fade" id="booking_modal" aria-hidden="true" aria-labelledby="booking_modal" tabindex="-1">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header  bg-success text-white">
+                <h5 class="modal-title" id="schedule_title">Title</h5>
+                <hr>
+                <b class="modal-title" id="schedule_date">Date</b> &nbsp; <b class="modal-title" id="schedule_time">Sub
+                    Title</b>
+            </div>
+            <div class="modal-body">
+                <div class="m-a30 wt-box border-2">
+                    {{-- date value set by jQuery --}}
+                    <input type="hidden" id="appointment_data" name="appointment_data">
+                    <input type="hidden" id="schedule_id" name="schedule">
+                    <form class="cons-contact-form" id="appointment_form">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <div class="input-group">
+                                        <input name="name" type="text" required=""
+                                            class="form-control customer_information" placeholder="Name">
                                     </div>
-                                </div>
-
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <div class="input-group">
-                                            <input name="phone" type="text" required=""
-                                                class="form-control customer_information" placeholder="Phone">
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <div class="input-group">
-                                            <input name="email" type="email" class="form-control customer_information"
-                                                placeholder="Email">
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <div class="input-group">
-                                            <input type="text" name="address" class="form-control customer_information"
-                                                placeholder="Address">
-
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <div class="input-group">
-                                            <input name="transaction_id" type="text" class="form-control"
-                                                placeholder="Transaction ID">
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <div class="input-group">
-                                            <input name="advance_amount" type="number" class="form-control"
-                                                placeholder="Advance amount">
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <div class="input-group">
-                                            <select name="service" class="form-control" required="">
-                                                <option value="" selected disabled>Please chose a service</option>
-                                                @foreach ($serviceCategories as $serviceCategory)
-                                                    <optgroup label="{{ $serviceCategory->name }}">
-                                                        @foreach ($serviceCategory->services as $service)
-                                                            <option value="{{ $service->id }}">{{ $service->name }}
-                                                            </option>
-                                                        @endforeach
-                                                    </optgroup>
-                                                @endforeach
-                                            </select>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-md-12 text-right">
-                                    <button name="submit" type="button"
-                                        class="btn waves-effect waves-light btn-outline-success"
-                                        id="appointment_submit_btn">Submit <i class="fa fa-angle-double-right"></i></button>
-                                    <button name="Resat" type="reset"
-                                        class="btn waves-effect waves-light btn-outline-info">Reset <i
-                                            class="fa fa-angle-double-right"></i></button>
                                 </div>
                             </div>
-                        </form>
-                    </div>
+
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <div class="input-group">
+                                        <input name="phone" type="text" required=""
+                                            class="form-control customer_information" placeholder="Phone">
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <div class="input-group">
+                                        <input name="email" type="email" class="form-control customer_information"
+                                            placeholder="Email">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <div class="input-group">
+                                        <input type="text" name="address" class="form-control customer_information"
+                                            placeholder="Address">
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <div class="input-group">
+                                        <input name="transaction_id" type="text" class="form-control"
+                                            placeholder="Transaction ID">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <div class="input-group">
+                                        <input name="advance_amount" type="number" class="form-control"
+                                            placeholder="Advance amount">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <div class="input-group">
+                                        <select name="service" class="form-control" required="">
+                                            <option value="" selected disabled>Please chose a service</option>
+                                            @foreach ($serviceCategories as $serviceCategory)
+                                            <optgroup label="{{ $serviceCategory->name }}">
+                                                @foreach ($serviceCategory->services as $service)
+                                                <option value="{{ $service->id }}">{{ $service->name }}
+                                                </option>
+                                                @endforeach
+                                            </optgroup>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-12 text-right">
+                                <button name="submit" type="button"
+                                    class="btn waves-effect waves-light btn-outline-success"
+                                    id="appointment_submit_btn">Submit <i class="fa fa-angle-double-right"></i></button>
+                                <button name="Resat" type="reset"
+                                    class="btn waves-effect waves-light btn-outline-info">Reset <i
+                                        class="fa fa-angle-double-right"></i></button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
 
 @push('head')
-    <link rel="stylesheet" href="{{ asset('assets/frontend/calender/style.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/frontend/calender/theme.css') }}">
-    <style>
-        .schedule-box:hover {
-            cursor: pointer;
-            border: 2px solid #06ec5c;
-            padding: 1px;
-            border-radius: 12px;
-        }
+<link rel="stylesheet" href="{{ asset('assets/frontend/calender/style.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/frontend/calender/theme.css') }}">
+<style>
+    .schedule-box:hover {
+        cursor: pointer;
+        border: 2px solid #06ec5c;
+        padding: 1px;
+        border-radius: 12px;
+    }
 
-        .ui-front {
-            z-index: 9999999 !important;
-        }
-
-    </style>
-    <link rel="stylesheet" href="//code.jquery.com/ui/1.13.0/themes/base/jquery-ui.css">
+    .ui-front {
+        z-index: 9999999 !important;
+    }
+</style>
+<link rel="stylesheet" href="//code.jquery.com/ui/1.13.0/themes/base/jquery-ui.css">
 @endpush
 
 @push('foot')
-    <script src="{{ asset('assets/frontend/calender/calendar.min.js') }}"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script src="{{ asset('assets/frontend/calender/calendar.min.js') }}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 
-    {{-- <script src="https://code.jquery.com/jquery-3.6.0.js"></script> --}}
-    <script src="https://code.jquery.com/ui/1.13.0/jquery-ui.js"></script>
+{{-- <script src="https://code.jquery.com/jquery-3.6.0.js"></script> --}}
+<script src="https://code.jquery.com/ui/1.13.0/jquery-ui.js"></script>
 
-    <script type="text/javascript">
-        function selectDate(date) {
+<script type="text/javascript">
+    function selectDate(date) {
             $('.calendar-wrapper').updateCalendarOptions({
                 date: date
             });
@@ -356,5 +355,5 @@
 
             }
         });
-    </script>
+</script>
 @endpush
